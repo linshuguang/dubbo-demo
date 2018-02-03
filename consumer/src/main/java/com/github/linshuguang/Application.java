d649c49823ff604e542453b646603a18bdbebc75
linshuguang <linshuguang@xiaomi.com>
2018-02-03 12:30:30 +0800

    success

--- consumer/src/main/java/com/github/linshuguang/Application.java
+++ consumer/src/main/java/com/github/linshuguang/Application.java
@@ -8,18 +8,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Application {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"app_dubbo_consumer.xml"});
-        context.start();
+        try {
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"app_dubbo_consumer.xml"});
+            context.start();
 
-        //获取远程服务代理
-        DemoService demonService = (DemoService) context.getBean("demonService");
-        // 调用方法
-        String sayHello = demonService.sayHello("world");
+            //获取远程服务代理
+            DemoService demonService = (DemoService) context.getBean("demoService");
+            // 调用方法
+            String sayHello = demonService.sayHello("world");
 
-        //显示结果
-        System.out.println(sayHello);
+            //显示结果
+            System.out.println(sayHello);
 
-        System.out.println("---------调用成功---------");
+            System.out.println("---------调用成功---------");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
 
     }
